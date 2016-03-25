ca0ea876cf625615fe401646240dd015bad325b2
taturou <taturou@gmail.com>
2016-03-25 22:13:08 +0900

    "Hello world" と表示するだけのアプリケーションを作成

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// module for application
+var app = require('app');
+
+// module for window
+var BrowserWindow = require('browser-window');
+
+// メインウィンドウはGCされないようにグローバル変数にする
+var mainWindow = null;
+
+// 全てのウィンドウが閉じたらアプリケーションを終了する
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+// Electronの初期化完了時に実行する
+app.on('ready', function() {
+    // ウィンドウ幅、高さ
+    mainWindow = new BrowserWindow({window: 800, height: 600});
+
+    // カレントディレクトリにある index.html を表示する
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    // ウィンドウを閉じたらアプリも終了
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
+
