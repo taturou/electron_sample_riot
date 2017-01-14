af8561885f52e39f85af60a4bb64963c2604f739
taturou <taturou@gmail.com>
2017-01-15 07:40:28 +0900

    タグファイルごとにフォルダ分けするようにした

--- src/render/js/main.js
+++ src/render/js/main.js
@@ -1,7 +1,7 @@
 import riot from 'riot';
 import '../tags/app.tag.js';
-import '../tags/versions.tag.js';
-import '../tags/businessman.tag.js';
+import '../tags/versions/index.tag.js';
+import '../tags/businessman/index.tag.js';
 riot.mount('app');
 
 import { install } from 'businessman'
