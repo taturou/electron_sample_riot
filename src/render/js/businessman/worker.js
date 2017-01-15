79535b3d6e6552dddc4a8c636829b7952a67be37
taturou <taturou@gmail.com>
2017-01-16 00:41:55 +0900

    aceを用いたエディタのサンプルを作成した fix #13

--- src/render/js/businessman/worker.js
+++ src/render/js/businessman/worker.js
@@ -26,4 +26,24 @@ worker.registerStore({
   }
 });
 
+worker.registerStore({
+  type: 'markdown',
+  state: '',
+  mutations: {
+    set: (state, text) => {
+      return text;
+    },
+  },
+  actions: {
+    set: (commit, text) => {
+      commit('set', text);
+    },
+  },
+  getters: {
+    text: (state) => {
+      return state;
+    }
+  }
+});
+
 worker.start();
--- src/render/js/main.js
+++ src/render/js/main.js
@@ -3,6 +3,9 @@ import '../tags/app.tag.js';
 import '../tags/versions/index.tag.js';
 import '../tags/businessman/index.tag.js';
 import '../tags/util/fontawesome.tag.js';
+import '../tags/markdown/index.tag.js';
+import '../tags/markdown/editor.tag.js';
+import '../tags/markdown/viewer.tag.js';
 riot.mount('app');
 
 import { install } from 'businessman'
