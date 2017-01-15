6eaa50c7f9574b0f9d19f220cda2b790e7178446
taturou <taturou@gmail.com>
2017-01-15 21:07:26 +0900

    font-awesomeを <fa> タグで表示できるようにした

--- src/render/js/main.js
+++ src/render/js/main.js
@@ -2,6 +2,7 @@ import riot from 'riot';
 import '../tags/app.tag.js';
 import '../tags/versions/index.tag.js';
 import '../tags/businessman/index.tag.js';
+import '../tags/util/fontawesome.tag.js';
 riot.mount('app');
 
 import { install } from 'businessman'
